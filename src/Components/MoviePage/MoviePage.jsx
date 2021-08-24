a596bac624db787440d24c7cc502af9a62036192
weevil <abhinavparagmishra23@gmail.com>
2021-08-24 18:17:52 +0530

    Complete movie page with trailer

--- src/Components/MoviePage/MoviePage.jsx
+++ src/Components/MoviePage/MoviePage.jsx
@@ -1,26 +1,74 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+import "./MoviePage.css"
 class MoviePage extends Component {
   state = { 
-    movie: {}
+    movie: {},
+    trailer: {}
    }
 
   async componentDidMount(){
     const id = this.props.location.state;
-    let response = await axios.get(`${process.env.REACT_APP_URL}/movie/${id}`, {
+    let movieDetailsResponse = await axios.get(`${process.env.REACT_APP_URL}/movie/${id}`, {
       params: {
         api_key: process.env.REACT_APP_KEY
       }
     });
-    let movie = response.data;
+    
+    let movieVideosResponse = await axios.get(`${process.env.REACT_APP_URL}/movie/${id}/videos`,{
+      params: {
+        api_key: process.env.REACT_APP_KEY,
+        language: 'en-US'
+      }
+    })
+
+    let trailer = {};
+    if(movieVideosResponse.data){
+      const allTrailers = movieVideosResponse.data.results.filter(videoObj => {
+        return videoObj.site == "YouTube" &&
+        videoObj.type == "Trailer"
+      });
+      if(allTrailers.length > 0){
+        trailer = allTrailers[0];
+      }
+    }
+    let movie = movieDetailsResponse.data;
     movie.poster_path = `${process.env.REACT_APP_IMAGE_URL}/${movie.poster_path}`;
     this.setState({
-      movie
+      movie,
+      trailer
     })
+    console.log(this.state);
   }
 
   render() { 
-    return ( <img src={this.state.movie.poster_path} alt="" /> );
+    let {title, tagline, vote_average, poster_path, overview} = this.state.movie;
+    const videoOptions = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // autoplay: 1
+      }
+    }
+    return ( 
+      <div className="movie-page">
+        <div className="movie-page-poster">
+          <img src={poster_path} alt="" />
+        </div>
+        <div className="movie-page-details">
+          <div className="movie-title-info">
+            <h1>{title}</h1>
+            <span>{tagline}</span>
+            <p>{vote_average} IMDB</p>
+            <p>{overview}</p>
+          </div>
+          <div className="movie-trailer">
+            <YouTube videoId={this.state.trailer.key} opts={videoOptions}></YouTube>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
  
